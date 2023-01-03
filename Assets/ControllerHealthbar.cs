11c8091651ee9e6a78d93c135e927c90a351717a
Wenish <Wenish@users.noreply.github.com>
2023-01-03 05:02:56 +0100

    Update ControllerHealthbar.cs

--- Assets/ControllerHealthbar.cs
+++ Assets/ControllerHealthbar.cs
@@ -6,12 +6,27 @@ using UnityEngine.UI;
 public class ControllerHealthbar : MonoBehaviour
 {
     public Slider slider;
+    public float updateSpeedSeconds = 0.2f;
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
     }
     public void SetHealth(int health)
     {
+        StartCoroutine(ChangeHealth(health));
+    }
+
+    private IEnumerator ChangeHealth(int health)
+    {
+        float preChangeHealth = slider.value;
+        float elapsed = 0f;
+        while (elapsed < updateSpeedSeconds)
+        {
+            elapsed += Time.deltaTime;
+            slider.value = Mathf.Lerp(preChangeHealth, health, elapsed / updateSpeedSeconds);
+            yield return null;
+        }
+
         slider.value = health;
     }
 }
