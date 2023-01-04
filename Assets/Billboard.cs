bff3aa53e1c0b939affb3f37d60c333993a416f9
Wenish <Wenish@users.noreply.github.com>
2023-01-04 16:36:16 +0100

    angle

--- Assets/Billboard.cs
+++ Assets/Billboard.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 public class Billboard : MonoBehaviour
 {
     private Camera CameraMain;
+    public Transform Target;
+    
+    [SerializeField]
+    private float Angle;
     void Awake()
     {
         CameraMain = Camera.main;
+    }
+    void Update()
+    {
+        CalculateAngle();
     }
     void LateUpdate()
     {
         transform.LookAt(transform.position + CameraMain.transform.forward);
         //transform.rotation = Quaternion.LookRotation(transform.position - CameraMain.transform.position);
+    }
+
+    private void CalculateAngle()
+    {
+        if (Target) {
+            Angle = Vector3.Angle((Target.position - CameraMain.transform.position), CameraMain.transform.forward);
+            Debug.Log("Angle:" + Angle);
+        }
     }
 }
