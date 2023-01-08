3933cc4c258d1fa0f55ac65802b1694c6d630b1d
Wenish <Wenish@users.noreply.github.com>
2023-01-08 07:43:40 +0100

    update

--- Assets/Objects/Orb/RotateSphere.cs
+++ Assets/Objects/Orb/RotateSphere.cs
@@ -6,8 +6,19 @@ public class RotateSphere : MonoBehaviour
 {
     public float rotationSpeed = 50f;
 
+    public float movementSpeed = 1f;
+    public float movementDistance = 0.25f;
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
     void Update()
     {
         transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+        transform.position = startPos + Vector3.up * Mathf.Sin(Time.time * movementSpeed) * movementDistance;
     }
 }
